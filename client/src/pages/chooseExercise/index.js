9e811e32295809b35e24f3dcd04f7087c8d369a2
StunnaDawg <jonsonallen9@gmail.com>
2023-06-22 17:29:49 -0300

    added more boiler plate

--- client/src/pages/chooseExercise/index.js
+++ client/src/pages/chooseExercise/index.js
@@ -3,9 +3,10 @@ import { Link } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
 // Display workouts from wokout database in categories
+// Focus on Displaying by Muscle group first, then Category, then difficulty 
 
     function getExercsies () {
-        
+
     }
 
     return (
