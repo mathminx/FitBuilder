8005e75927576ea288c1aa171ca3f3470970dbd1
StunnaDawg <jonsonallen9@gmail.com>
2023-06-22 17:19:28 -0300

    chooseExercsie index

---
+++ client/src/pages/chooseExercise/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+
+// Display workouts from wokout database in categories
+
+    function getExercsies () {
+        
+    }
+
+    return (
+        <>
+        {/* */}
+        </>
+      );
