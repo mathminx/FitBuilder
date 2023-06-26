172979cb366563aa73751e2bce42a68049e41f19
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-06-26 11:26:15 -0300

    added warning message if user tries to access dashboard without loggin in using the navbar

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -9,11 +9,27 @@ import {
 } from "@ant-design/icons";
 import { LoginModal, SignupModal } from "./LoginSignup";
 import Auth from "../../utils/auth";
+import { useNavigate } from "react-router-dom";
+import { Alert, Space } from "antd";
+
+const WarningComponent = () => (
+  <Space
+    direction="vertical"
+    style={{
+      width: "100%",
+    }}
+  >
+    <Alert message="Warning: You must be logged in to access the dashboard, please login to continue. If you do not have an account, please sign up!" banner closable />
+  </Space>
+);
 
 const SimpleNavbar = () => {
+    const navigate = useNavigate();
 
   const [loginVisible, setLoginVisible] = useState(false);
   const [signupVisible, setSignupVisible] = useState(false);
+  const [showWarning, setShowWarning] = useState(false);
+
 
   const showLoginModal = () => {
     setLoginVisible(true);
@@ -35,33 +51,71 @@ const SimpleNavbar = () => {
     Auth.logout();
   };
 
+  const takeHome = () => {
+    navigate('/')
+  };
+
+const takeToDashboard = () => {
+  if (Auth.loggedIn()) {
+    navigate("/dashboard");
+  } else {
+    setShowWarning(true);
+  }
+};
+
+
   return (
-   <>
-    <Menu mode="horizontal">
-      <Menu.Item key="home" icon={<HomeOutlined />}>
-        Home
-      </Menu.Item>
-      <Menu.Item key="dashboard" icon={<DashboardOutlined />}>
-        Dashboard
-      </Menu.Item>
+    <>
+      {showWarning && <WarningComponent />}
+      <Menu mode="horizontal">
+        <Menu.Item key="home" icon={<HomeOutlined />} onClick={takeHome}>
+          Home
+        </Menu.Item>
+        <Menu.Item
+          key="dashboard"
+          icon={<DashboardOutlined />}
+          onClick={takeToDashboard}
+        >
+          Dashboard
+        </Menu.Item>
         {Auth.loggedIn() ? (
-          <Menu.Item key="logout" icon={<LogoutOutlined />} onClick={handleLogout}>
+          <Menu.Item
+            key="logout"
+            icon={<LogoutOutlined />}
+            onClick={handleLogout}
+          >
             Logout
           </Menu.Item>
         ) : (
           <>
-            <Menu.Item key="login" icon={<LoginOutlined />} onClick={showLoginModal}>
+            <Menu.Item
+              key="login"
+              icon={<LoginOutlined />}
+              onClick={showLoginModal}
+            >
               Login
             </Menu.Item>
-            <Menu.Item key="signup" icon={<UserAddOutlined />} onClick={showSignupModal}>
+            <Menu.Item
+              key="signup"
+              icon={<UserAddOutlined />}
+              onClick={showSignupModal}
+            >
               Signup
             </Menu.Item>
           </>
         )}
-       </Menu>
-      <LoginModal visible={loginVisible} handleOk={hideLoginModal} handleCancel={hideLoginModal} />
-      <SignupModal visible={signupVisible} handleOk={hideSignupModal} handleCancel={hideSignupModal} />
-   </>
+      </Menu>
+      <LoginModal
+        visible={loginVisible}
+        handleOk={hideLoginModal}
+        handleCancel={hideLoginModal}
+      />
+      <SignupModal
+        visible={signupVisible}
+        handleOk={hideSignupModal}
+        handleCancel={hideSignupModal}
+      />
+    </>
   );
 };
 
