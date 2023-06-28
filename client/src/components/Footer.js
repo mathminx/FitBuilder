e4cd025c78585c7188181d62315832171d272b34
Pilotguide9897 <jaredryan1997@hotmail.com>
2023-06-28 11:42:57 -0300

    changes to the page to make the footer display at the bottom

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,19 +2,32 @@ import React from 'react';
 
 const styles = {
   footerStyle: {
-    alignItems: 'center',
-    display: 'flex',
-    paddingLeft: '15px',
+    position: "fixed", // Fix position
+    left: "0", // Align left
+    bottom: "0", // Align bottom
+    width: "100%", // Take full width
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
     height: "55px",
     background: "#193381",
-    margin: "0px"
+    color: "gray",
+    fontSize: "12px",
+    paddingLeft: "15px",
+    margin: "0px",
   },
-}
+};
 
 function Footer() {
   return (
-    <footer style={styles.footerStyle} className="footer"></footer>
+    <footer style={styles.footerStyle} className="footer">
+      <p>
+        © 2023 FitBuilder. All rights reserved. Powering your fitness journey,
+        one workout at a time.
+      </p>
+    </footer>
   );
 }
 
 export default Footer;
+
